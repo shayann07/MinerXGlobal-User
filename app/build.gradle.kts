dfb243d5d72b9f417fb2e629163d058b34553fe2
Zubair Shafi <zubair38421@gmail.com>
2025-09-29 20:37:02 +0500

    v8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.minerxgloble.minerxgloble"
         minSdk = 24
         targetSdk = 35
-        versionCode = 7
-        versionName = "7.0"
+        versionCode = 8
+        versionName = "8.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
