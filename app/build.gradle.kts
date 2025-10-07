2be953af4b7ca10c2a22c5a39cfa0df758d22c25
Zubair Shafi <zubair38421@gmail.com>
2025-10-07 19:59:43 +0500

    v10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.minerxgloble.minerxgloble"
         minSdk = 24
         targetSdk = 35
-        versionCode = 9
-        versionName = "9.0"
+        versionCode = 10
+        versionName = "10.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
